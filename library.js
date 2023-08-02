297c70cf06e76c7266ee0881f69cb4b76dfcc41e
Josh Merhar <122338744+JoshMerhar@users.noreply.github.com>
2023-08-01 23:58:28 -0400

    Write functions to display cards from array and to change status

--- library.js
+++ library.js
@@ -1,6 +1,17 @@
-const library = document.querySelector(".library");
+const mainContent = document.querySelector(".library");
 
-let myLibrary = [];
+let myLibrary = [{title: "Some Book", author: "Some Person", pages: 830, status: "Read"}];
+displayLibrary();
+
+const newBookForm = document.querySelector(".new-book");
+const addBookButton = document.querySelector("#new-book-button");
+addBookButton.addEventListener("click", () => newBookForm.style.display = "block");
+
+const closeForm = document.querySelector(".close-form");
+closeForm.addEventListener("click", () => newBookForm.style.display = "none")
+
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", addBookToLibrary);
 
 class Book {
     constructor(title, author, pages, status) {
@@ -15,22 +26,59 @@ function addBookToLibrary(event) {
     if (form.title.value && form.author.value && form.pages.value) {
         event.preventDefault();
         const newBook = new Book(title, author, pages, status);
-        const createBook = document.createElement("div");
-        createBook.classList.add("book-card");
-        createBook.innerHTML = 
-            `<div class="book-info">
-                <div class="book-title">${newBook.title}</div>
-                <div class="book-author">by ${newBook.author}</div>
-                <div class="book-pages">${newBook.pages} pages</div>
-            </div>
-            <div class="book-buttons">
-                <button class="status-button">${newBook.status}</button>
-                <button class="remove-book-button">Remove</button>
-            </div>`;
-        library.appendChild(createBook);
         myLibrary.push(newBook);
         resetForm();
+        resetPage();
+        displayLibrary();
     }
+}
+
+function displayLibrary() {
+    if (myLibrary) {
+        for (let i = 0; i < myLibrary.length; i++) {
+            const createBook = document.createElement("div");
+            createBook.classList.add("book-card");
+            createBook.innerHTML = 
+                `<div class="book-info">
+                    <div class="book-title">${myLibrary[i].title}</div>
+                    <div class="book-author">by ${myLibrary[i].author}</div>
+                    <div class="book-pages">${myLibrary[i].pages} pages</div>
+                </div>
+                <div class="book-buttons">
+                    <button class="status-button">${myLibrary[i].status}</button>
+                    <button class="remove-book-button">Remove</button>
+                </div>`;
+            myLibrary[i].id = i;
+            mainContent.appendChild(createBook);
+            setStatus();
+        }
+    }
+}
+
+function setStatus() {
+    const statusButtons = document.querySelectorAll(".status-button");
+    statusButtons.forEach(statusButton => {
+        if (statusButton.innerHTML === "Read") {
+            statusButton.classList.add("status-button-read");
+        } else {
+            statusButton.classList.add("status-button-unread");
+        }
+    });
+    statusButtons.forEach(statusButton => statusButton.addEventListener("click", changeStatus))
+}
+
+function changeStatus() {
+    const statusButtons = document.querySelectorAll(".status-button");
+    statusButtons.forEach(statusButton => {
+        if (statusButton.innerHTML === "Read") {
+            statusButton.classList.remove("status-button-read");
+            statusButton.classList.add("status-button-unread");
+            statusButton.innerHTML = "Not read";
+        } else {
+            statusButton.classList.remove("status-button-unread");
+            statusButton.classList.add("status-button-read");
+            statusButton.innerHTML = "Read";
+        }});
 }
 
 function resetForm() {
@@ -41,12 +89,9 @@ function resetForm() {
     newBookForm.style.display = "none"
 }
 
-const newBookForm = document.querySelector(".new-book");
-const addBookButton = document.querySelector("#new-book-button");
-addBookButton.addEventListener("click", () => newBookForm.style.display = "block");
-
-const closeForm = document.querySelector(".close-form");
-closeForm.addEventListener("click", () => newBookForm.style.display = "none")
-
-const submitButton = document.querySelector("#submit");
-submitButton.addEventListener("click", addBookToLibrary);
+function resetPage() {
+    const allBooks = document.querySelectorAll(".book-card");
+    allBooks.forEach(book => {
+        book.remove();
+    })
+}
